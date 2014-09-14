080c54897d616eea1910b9787f227e22487ff230
Bernardo J.B. Schmitt <bernardo.jb.schmitt@gmail.com>
2014-09-14 14:40:31 -0300

    First version

---
+++ bin/AMFM_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+Script to test the AMFM_decompy package.
+
+Version 1.0.0
+10/Sep/2014 Bernardo J.B. Schmitt - bernardo.jb.schmitt@gmail.com
+"""
+
+import amfm_decompy.pYAAPT as pyaapt
+import amfm_decompy.pyQHM as pyqhm
+import os.path
+
+# Declare the variables.
+file_name = os.path.dirname(amfm_decompy.__file__)+os.sep+"sample.wav"
+window_duration = 0.015
+nharm_max = 25
+SNR = float('Inf')
+
+# Create the signal object.
+signal = pyqhm.SignalObj(file_name)
+
+# Create the window object.
+window = pyqhm.SampleWindow(window_duration, signal.fs)
+
+# Create the pitch object and calculate its attributes.
+pitch = pyaapt.yaapt(signal.data, signal.fs)
+
+# Set the number of modulated components.
+signal.set_nharm(pitch.values, nharm_max)
+
+# Check if gaussian noise has to be added.
+if SNR != float('Inf'):
+    signal.noiser(pitch.values, SNR)
+
+# Perform the QHM extraction.
+QHM = pyqhm.qhm(signal, pitch, window, 0.001, N_iter = 3, phase_tech = 'phase')
+
+print ("QHM SRER: %s" % (QHM.SRER))
+
+# Perform the aQHM extraction.
+aQHM = pyqhm.aqhm(signal, QHM, pitch, window, 0.001, N_iter = 3, N_runs = 2,
+            phase_tech = 'phase')
+
+print ("aQHM SRER: %s" % (aQHM.SRER))
+
+# Perform the eaQHM extraction.
+eaQHM = pyqhm.eaqhm(signal, aQHM, pitch, window, 0.001, N_iter=3, N_runs=2,
+              phase_tech = 'phase')
+
+print ("eaQHM SRER: %s" % (eaQHM.SRER))
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='AMFM_decompy',
+    version='1.0.0',
+    author='Bernardo J.B. Schmitt',
+    author_email='bernardo.jb.schmitt@gmail.com',
+    packages=['amfm_decompy'],
+    scripts=['bin/AMFM_test.py'],
+    url='https://github.com/bjbschmitt/AMFM_decompy/',
+    license='LICENSE.txt',
+    description='Package to decompose the voiced part of a speech signal into \
+                    its modulated components, aka AM-FM decomposition.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "itertools"
+    ],
+)
+
