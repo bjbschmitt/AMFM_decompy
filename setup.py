ade2f27ed7fa203caace8f4b679965cf3238778b
bjbschmitt <bernardo.jb.schmitt@gmail.com>
2014-09-16 01:40:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email='bernardo.jb.schmitt@gmail.com',
     packages=['amfm_decompy'],
     scripts=['bin/AMFM_test.py'],
+    package_data = {'amfm_decompy': ['*.wav']},
     url='https://github.com/bjbschmitt/AMFM_decompy/',
     license='LICENSE.txt',
     description='Package to decompose the voiced part of a speech signal into \
